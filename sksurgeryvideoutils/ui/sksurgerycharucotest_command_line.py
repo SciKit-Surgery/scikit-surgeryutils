8a08a7347e8ceb2746118c75a5d545080ba36511
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-12-22 15:20:15 +0000

    Issue #3: Make command line args for sksurgerycharucotest similar to sksurgeryvideolag.

--- sksurgeryvideoutils/ui/sksurgerycharucotest_command_line.py
+++ sksurgeryvideoutils/ui/sksurgerycharucotest_command_line.py
@@ -18,23 +18,35 @@ def main(args=None):
                         type=int,
                         help="Camera index.")
 
-    parser.add_argument("-x", "--x_squares",
+    parser.add_argument("-x", "--x_size",
                         required=False,
-                        default=13,
+                        default=640,
                         type=int,
-                        help="Number of squares in X direction.")
+                        help="Image width")
 
-    parser.add_argument("-y", "--y_squares",
+    parser.add_argument("-y", "--y_size",
+                        required=False,
+                        default=480,
+                        type=int,
+                        help="Image height")
+
+    parser.add_argument("-i", "--horizontal",
                         required=False,
                         default=10,
                         type=int,
-                        help="Number of squares in Y direction.")
+                        help="Number of squares horizontally.")
+
+    parser.add_argument("-j", "--vertical",
+                        required=False,
+                        default=13,
+                        type=int,
+                        help="Number of squares vertically.")
 
     parser.add_argument("-d", "--dictionary",
                         required=False,
                         default=2,
                         type=int,
-                        help="ArUco dictionary enum.")
+                        help="ArUco dictionary enum. (see online)")
 
     version_string = __version__
     friendly_version_string = version_string if version_string else 'unknown'
@@ -46,7 +58,9 @@ def main(args=None):
     args = parser.parse_args(args)
 
     run_demo(args.camera,
-             args.x_squares,
-             args.y_squares,
+             args.x_size,
+             args.y_size,
+             args.horizontal,
+             args.vertical,
              args.dictionary
              )
