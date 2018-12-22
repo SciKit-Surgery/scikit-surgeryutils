d7af23aae2a2b503242e1ba06476d0c62621440e
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-12-22 07:07:20 +0000

    Issue #3: Fix editing error in test.

--- tests/test_sksurgeryvideolag.py
+++ tests/test_sksurgeryvideolag.py
@@ -2,10 +2,10 @@
 
 """sksurgeryvideolag tests"""
 
-from sksurgeryvideoutils.ui.sksurgeryvideolag_ import run_demo
+from sksurgeryvideoutils.ui.sksurgeryvideolag_demo import run_demo
 
 
 def test_sksurgeryvideolag():
     # placeholder for now.
-    assert True
+    pass
 
