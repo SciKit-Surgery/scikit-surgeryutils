e6f6149131e1492a7ffe6d297c0b664d225e3534
Matt Clarkson <m.clarkson@ucl.ac.uk>
2022-05-27 12:09:53 +0100

    Issue #46: Get video calibration GUI up, ensure quit button works.

--- sksurgeryutils/ui/sksurgeryvideocalibration_app.py
+++ sksurgeryutils/ui/sksurgeryvideocalibration_app.py
@@ -1,27 +1,299 @@
 # coding=utf-8
 
-""" Functions to run video calibration. """
+""" Functions to run video calibration, in interactive or non-interactive mode. """
 
 import os
+import sys
 import cv2
+import time
 from PySide2 import QtWidgets
+from PySide2.QtWidgets import QWidget, QHBoxLayout, QApplication
+from PySide2.QtCore import QTimer
+from sksurgeryvtk.widgets.vtk_overlay_window import VTKOverlayWindow
 import sksurgeryimage.calibration.chessboard_point_detector as cpd
 import sksurgerycalibration.video.video_calibration_driver_mono as mc
-import sksurgeryutils.common_overlay_apps as coa
 
 # pylint:disable=too-many-nested-blocks,too-many-branches
 
 
-class CalibrationWindow(coa.OverlayBaseApp):
+class CalibrationDriver:
+    """
+    Main calibration logic in a separate class, so it can be
+    used either in interactive (widget) mode, or not.
+    """
+    def __init__(self,
+                 configuration=None,
+                 save_dir=None,
+                 prefix=None
+                 ):
+        """
+        Constructor must throw if anything at all wrong.
+        """
+        self.save_dir = save_dir
+        self.prefix = prefix
+
+        if self.prefix is not None and self.save_dir is None:
+            self.save_dir = "./"
+
+        if configuration is None:
+            configuration = {}
+
+        # For now just doing chessboards.
+        # The underlying framework works for several point detectors,
+        # but each would have their own parameters etc.
+        method = configuration.get("method", "chessboard")
+        if method != "chessboard":
+            raise ValueError("Only chessboard calibration is currently supported")
+
+        source = configuration.get("source", 0)
+        corners = configuration.get("corners", [14, 10])
+        corners = (corners[0], corners[1])
+        size = configuration.get("square size in mm", 3)
+        window_size = configuration.get("window size", None)
+        min_num_views = configuration.get("minimum number of views", 5)
+
+        self.cap = cv2.VideoCapture(source)
+        if not self.cap.isOpened():
+            raise RuntimeError("Failed to open camera.")
+
+        if window_size is not None:
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, window_size[0])
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, window_size[1])
+            print("Video feed set to ("
+                  + str(window_size[0]) + " x " + str(window_size[1]) + ")")
+        else:
+            width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            print("Video feed defaults to ("
+                  + str(width) + " x " + str(height) + ")")
+
+        self.detector = cpd.ChessboardPointDetector(corners, size)
+        self.calibrator = mc.MonoVideoCalibrationDriver(self.detector,
+                                                        corners[0] * corners[1])
+
+        self.frame_ok = False
+        self.frame = None
+        self.annotated = None
+
+        print("Minimum number of views to calibrate:" + str(min_num_views))
+
+    def shutdown(self):
+        """
+        Try to do any cleanup properly. Releases video source.
+        """
+        self.cap.release()
+
+    def grab_frame(self):
+        """
+        Captures image.
+
+        :return: frame_ok, frame
+        :rtype: bool, numpy.ndarray
+        """
+        self.frame_ok, self.frame = self.cap.read()
+
+        if not self.frame_ok:
+            print("Reached end of video source or read failure.")
+
+        return self.frame_ok, self.frame
+
+    def extract_points(self):
+        """
+        Extracts the points from the image.
+
+        :return: number_points, annotated
+        :rtype: int, numpy.ndarray
+        """
+        annotated = None
+        number_points = self.calibrator.grab_data(self.frame)
+
+        if number_points > 0:
+            img_pts = self.calibrator.video_data.image_points_arrays[-1]
+            self.annotated = cv2.drawChessboardCorners(self.frame,
+                                                       self.corners,
+                                                       img_pts,
+                                                       number_points)
+            number_of_views = self.calibrator.get_number_of_views()
+            print("Number of frames = " + str(number_of_views))
+
+            if number_of_views >= self.min_num_views:
+
+                proj_err, params = self.calibrator.calibrate()
+                print("Reprojection (2D) error is:" + str(proj_err))
+                print("Intrinsics are:")
+                print(params.camera_matrix)
+                print("Distortion matrix is:")
+                print(params.dist_coeffs)
+
+                if self.save_dir is not None:
+
+                    if not os.path.isdir(self.save_dir):
+                        os.makedirs(self.save_dir)
+
+                    self.calibrator.save_data(self.save_dir,
+                                              self.prefix)
+                    self.calibrator.save_params(self.save_dir,
+                                                self.prefix)
+        else:
+            print("Failed to detect points")
+
+        return number_points, annotated
+
+
+class CalibrationWidget(QWidget):
+    """
+    Widget to provide calibration in interactive mode.
+    """
     def __init__(self,
                  configuration=None,
                  save_dir=None,
                  prefix=None):
         """
-
+        Constructor creates the internal CalibrationDriver.
         """
+        super().__init__()
+        self.driver = CalibrationDriver(configuration,
+                                        save_dir,
+                                        prefix)
 
-def run_video_calibration(configuration = None, save_dir = None, prefix = None):
+        self.layout = QHBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+
+        self.vtk_overlay_window = VTKOverlayWindow()
+        self.layout.addWidget(self.vtk_overlay_window)
+
+        self.keypress_delay_in_milliseconds = configuration.get("keypress delay", 10)
+
+        self.vtk_overlay_window.AddObserver("KeyPressEvent",
+                                            self.on_key_press_event)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_view)
+
+        self.update_rate = 30
+        self.do_capture = False
+
+        print("Press 'q' to quit and 'c' to capture an image.")
+
+    def start(self):
+        """
+        Starts the timer, which repeatedly triggers the update_view() method.
+        """
+        self.timer.start(int(1000.0 / self.update_rate))
+
+    def stop(self):
+        """
+        Stops the timer.
+        """
+        self.timer.stop()
+
+    def finalize(self):
+        """
+        Make sure that the VTK Interactor terminates nicely, otherwise
+        it can throw some error messages, depending on the usage.
+        """
+        self.vtk_overlay_window._RenderWindow.Finalize()
+
+    def on_key_press_event(self, obj, event):
+        """
+        Called when a key is pressed, if 'q', exit the app, if 'c' capture image.
+        """
+        if self.vtk_overlay_window.GetKeySym() == 'q':
+            print("Detected 'q' key press, exiting.")
+            self.on_exit_selected()
+        elif self.overlay_window.GetKeySym() == 'c':
+            print("Detected 'c' key press, capturing image.")
+            self.do_capture = True
+
+    def on_exit_selected(self):
+        """
+        Opportunity to do any clear up, before exiting app.
+
+        Stops timer, shutsdown camera feed, and asks VTK to kill window/app.
+        """
+        self.stop()
+        self.driver.shutdown()
+        self.finalize()
+        QApplication.quit()
+
+    def update_view(self):
+        """
+        Called by base class, to do the main UI window update.
+        """
+        frame_ok, frame = self.driver.grab_frame()
+
+        if not frame_ok:
+            print("Failed to read frame")
+            return
+
+        self.vtk_overlay_window.set_video_image(frame)
+
+        if self.do_capture:
+
+            number_of_points, annotated_image = self.driver.extract_points()
+            self.do_capture = False
+
+            if number_of_points < 1:
+                print("Failed to detect points")
+                return
+
+            self.vtk_overlay_window.set_video_image(annotated_image)
+            self.vtk_overlay_window.Render()
+            self.update()
+
+            time.sleep(self.keypress_delay_in_milliseconds / 1000.0)
+
+        else:
+            self.vtk_overlay_window.Render()
+            self.update()
+
+
+class CalibrationManager:
+    """
+    For non-interactive mode. Just reads from video source
+    in a loop, and extracts points every few frames (see config file).
+    """
+    def __init__(self,
+                 configuration=None,
+                 save_dir=None,
+                 prefix=None
+                 ):
+        """
+        Constructor creates the internal CalibrationDriver.
+        """
+        self.driver = CalibrationDriver(configuration,
+                                        save_dir,
+                                        prefix)
+
+        self.sample_frequency = configuration.get("sample frequency", 1)
+        self.frames_sampled = 0
+
+    def run(self):
+        """
+        Main loop that will process frames from the video source, until no frames.
+        """
+        while True:
+
+            frame_ok, frame = self.driver.grab_frame()
+
+            if not frame_ok:
+                print("Reached end of video source or read failure.")
+                break
+
+            self.frames_sampled += 1
+
+            if self.frames_sampled % self.sample_frequency == 0:
+
+                number_of_points, _ = self.driver.extract_points()
+
+                if number_of_points < 1:
+                    print("Failed to detect points")
+
+
+def run_video_calibration(configuration=None,
+                          save_dir=None,
+                          prefix=None):
     """
     Performs Video Calibration using OpenCV
     source and scikit-surgerycalibration.
@@ -33,107 +305,22 @@ def run_video_calibration(configuration = None, save_dir = None, prefix = None):
 
     :raises ValueError: if method is not supported
     """
-    if prefix is not None and save_dir is None:
-        save_dir = "./"
-
-    if configuration is None:
-        configuration = {}
-
-    # For now just doing chessboards.
-    # The underlying framework works for several point detectors,
-    # but each would have their own parameters etc.
-    method = configuration.get("method", "chessboard")
-    if method != "chessboard":
-        raise ValueError("Only chessboard calibration is currently supported")
-
-    source = configuration.get("source", 0)
-    corners = configuration.get("corners", [14, 10])
-    corners = (corners[0], corners[1])
-    size = configuration.get("square size in mm", 3)
-    window_size = configuration.get("window size", None)
-    min_num_views = configuration.get("minimum number of views", 5)
-    keypress_delay = configuration.get("keypress delay", 10)
     interactive = configuration.get("interactive", True)
-    sample_frequency = configuration.get("sample frequency", 1)
 
-    cap = cv2.VideoCapture(source)
-    if not cap.isOpened():
-        raise RuntimeError("Failed to open camera.")
+    if interactive:
 
-    if window_size is not None:
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, window_size[0])
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, window_size[1])
-        print("Video feed set to ("
-              + str(window_size[0]) + " x " + str(window_size[1]) + ")")
+        app = QtWidgets.QApplication([])
+
+        widget = CalibrationWidget(configuration,
+                                   save_dir,
+                                   prefix)
+        widget.show()
+        widget.start()
+        sys.exit(app.exec_())
+
     else:
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        print("Video feed defaults to ("
-              + str(width) + " x " + str(height) + ")")
 
-    detector = cpd.ChessboardPointDetector(corners, size)
-    calibrator = mc.MonoVideoCalibrationDriver(detector,
-                                               corners[0] * corners[1])
-
-    print("Press 'q' to quit and 'c' to capture an image.")
-    print("Minimum number of views to calibrate:" + str(min_num_views))
-
-    app = QtWidgets.QApplication([])
-    live_widget = vow.VTKOverlayWindow()
-    live_widget.show()
-    captured_widget = vow.VTKOverlayWindow()
-    app.exec_()
-
-    frames_sampled = 0
-    while True:
-        frame_ok, frame = cap.read()
-
-        key = None
-        frames_sampled += 1
-
-        if not frame_ok:
-            print("Reached end of video source or read failure.")
-            key = ord('q')
-        else:
-            if interactive:
-                live_widget.set_video_image(frame)
-                key = cv2.waitKey(keypress_delay)
-            else:
-                if frames_sampled % sample_frequency == 0:
-                    key = ord('c')
-
-        if key == ord('q'):
-            break
-        if key == ord('c'):
-            number_points = calibrator.grab_data(frame)
-            if number_points > 0:
-                img_pts = calibrator.video_data.image_points_arrays[-1]
-                img = cv2.drawChessboardCorners(frame, corners,
-                                                img_pts,
-                                                number_points)
-                if interactive:
-                    captured_widget.set_video_image(img)
-                    captured_widget.show()
-
-                number_of_views = calibrator.get_number_of_views()
-                print("Number of frames = " + str(number_of_views))
-
-                if number_of_views >= min_num_views:
-                    proj_err, params = calibrator.calibrate()
-                    print("Reprojection (2D) error is:" + str(proj_err))
-                    print("Intrinsics are:")
-                    print(params.camera_matrix)
-                    print("Distortion matrix is:")
-                    print(params.dist_coeffs)
-
-                    if save_dir is not None:
-                        if not os.path.isdir(save_dir):
-                            os.makedirs(save_dir)
-
-                        calibrator.save_data(save_dir, prefix)
-                        calibrator.save_params(save_dir, prefix)
-            else:
-                print("Failed to detect points")
-
-    cap.release()
-    cv2.destroyAllWindows()
+        manager = CalibrationManager(configuration,
+                                     save_dir,
+                                     prefix)
+        manager.run()
