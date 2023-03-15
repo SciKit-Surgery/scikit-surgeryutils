bbccad13025b9d6d8d0cb18d1c682c22c12bd65f
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-03-15 09:36:49 +0000

    comment pytest for darwin runner in test_sksurgerytextoverlay (#53)

--- tests/test_sksurgerytextoverlay.py
+++ tests/test_sksurgerytextoverlay.py
@@ -24,8 +24,9 @@ skip_pytest_in_linux_and_none_ci = pytest.mark.skipif(
 def test_sksurgerytextoverlay():
     """ Basic test to run the widget and make sure everything loads OK."""
 
-    if sys.platform == "darwin":
-        pytest.skip("Test not working on Mac runner")
+    print(f'platform.system(): {platform.system()}')
+    # if sys.platform == "darwin":
+    #     pytest.skip("Test not working on Mac runner")
 
     # Use input video rather than camera to test
     input_file = 'tests/data/test_video.avi'
